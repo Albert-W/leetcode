0e02b524cb946aec5649e9f61a087ae0e9b2314e
wuyiqin <919911537@qq.com>
2018-03-21 14:36:55 +0000

    add an O(nlgn) method.

--- src/sum_01.java
+++ src/sum_01.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class sum_01 {
     public int[] twoSum(int[] nums, int target) {
         int[] result= new int[2];
@@ -12,4 +15,89 @@ public class sum_01 {
         }
         return result;
     }
+}
+
+
+class sum_02{
+    /**
+     *
+     * @param ob the object two dimentional array
+     * @param order the order of rules you like to be sort by.
+     */
+    public static void sort(int[][] ob, final int[] order) {
+        Arrays.sort(ob, new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                int[] one = (int[]) o1;
+                int[] two = (int[]) o2;
+                for (int i = 0; i < order.length; i++) {
+                    int k = order[i];
+                    if (one[k] > two[k]) {
+                        return 1;
+                    } else if (one[k] < two[k]) {
+                        return -1;
+                    } else {
+                        continue;  //如果按一条件比较结果相等，就使用第二个条件进行比较。
+                    }
+                }
+                return 0;
+            }
+        });
+    }
+
+
+    /**
+     * the member k means the kth row you what to be sort by.
+     */
+    class mycom implements Comparator<Object>{
+        int k=0;
+        mycom(int k){
+            this.k=k;
+        }
+        public int compare(Object o1, Object o2) {
+            int[] one = (int[]) o1;
+            int[] two = (int[]) o2;
+            if(one[k]>two[k])return 1;
+            else if(one[k]<two[k])return -1;
+            return 0;
+        }
+    }
+
+    /**
+     *
+     * @param ob the object you want to sort.
+     */
+     void sort(int [][] ob){
+        Arrays.sort(ob, new mycom(0));
+
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result= new int[2];
+        //build the two dimensional array.
+        int[][] temp = new int [nums.length][2];
+        for(int i=0;i<nums.length;i++){
+            temp[i][0]=nums[i];
+            temp[i][1]=i;
+        }
+        //sort the array.
+        sort(temp);
+        //find the pairs;
+        int i=0,j=temp.length-1;
+        while(i<j){
+            if(temp[i][0]+temp[j][0]==target){
+                result[0]=temp[i][1];
+                result[1]=temp[j][1];
+                return result;
+            }
+            else if(temp[i][0]+temp[j][0]<target){
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return result;
+    }
+
 }
