e694282e14c9e4d453418f7ed89ea11bae87b456
wuyiqin <919911537@qq.com>
2018-03-27 09:50:49 +0100

    2.add two numbers.java

--- src/LongestSubstring.java
+++ src/LongestSubstring.java
@@ -1,2 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstring {
+    /**
+     * @param s the target string.
+     * @return the length of the longest substring
+     * <p>
+     * method: use int[256] as a map to check whether the character exists or not.
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int j = i;
+            int count=0;
+            byte[] map = new byte[256];
+            while (j < s.length() && map[s.charAt(j)] == 0) {
+
+                count++;
+                map[s.charAt(j)] = 1;
+                j++;
+            }
+            if(count>length)length=count;
+        }
+        return length;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        for(int i=0;i<100;i++){
-            Random random = new Random();
-            System.out.println(random.nextInt());
-        }
+//        for(int i=0;i<100;i++){
+//            Random random = new Random();
+//            System.out.println(random.nextInt());
+//
+//
+//
+//            //System.out.println(Boolean.SIZE/8);
+//        }
+        System.out.println(Integer.SIZE/8);
 
 
     }
