e0d41ccc6654eb42d2323245a887da0677f9b5ce
wuyiqin <919911537@qq.com>
2018-03-27 10:32:39 +0100

    3.add LongestSubstring

--- src/LongestSubstring.java
+++ src/LongestSubstring.java
@@ -7,12 +7,13 @@ public class LongestSubstring {
      * @return the length of the longest substring
      * <p>
      * method: use int[256] as a map to check whether the character exists or not.
+     * use while() to reduce the time complexity to O(n);
      */
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring_1(String s) {
         int length = 0;
         for (int i = 0; i < s.length(); i++) {
             int j = i;
-            int count=0;
+            int count = 0;
             byte[] map = new byte[256];
             while (j < s.length() && map[s.charAt(j)] == 0) {
 
@@ -20,9 +21,50 @@ public class LongestSubstring {
                 map[s.charAt(j)] = 1;
                 j++;
             }
-            if(count>length)length=count;
+            if (count > length) length = count;
         }
         return length;
 
+    }
+
+    public int lengthOfLongestSubstring_2(String s) {
+        int length = 0;
+        byte[] map = new byte[256];
+        int i = 0, j = 0;
+        int count = 0;
+        for (; i < s.length(); i++) {
+
+            while (j < s.length() && map[s.charAt(j)] == 0) {
+                count++;
+                map[s.charAt(j)] = 1;
+                j++;
+            }
+            if (count > length) length = count;
+            count--;
+            map[s.charAt(i)] = 0;
+        }
+        return length;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+        byte[] map = new byte[256];
+        int i = 0, j = 0;
+        int count = 0;
+
+        while (i < s.length() && j < s.length()) {
+            if(map[s.charAt(j)]==0){
+                count++;
+                map[s.charAt(j)] = 1;
+                j++;
+            } else {
+                if (count > length) length = count;
+                count--;
+                map[s.charAt(i)] = 0;
+                i++;
+            }
+        }
+        if (count > length) length = count;
+        return length;
     }
 }
