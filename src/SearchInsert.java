6ac0260879ddb54fd0e28adfed4d6df522351077
Albert-W <wuyichang1990@gmail.com>
2018-04-27 11:35:11 +0100

    amend

--- src/SearchInsert.java
+++ src/SearchInsert.java
@@ -25,26 +25,26 @@ Output: 0
 
  */
 public class SearchInsert {
-  public int searchInsert2(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] >= target)
-        return i;
-    }
-    return nums.length;
-  }
+	public int searchInsert2(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] >= target)
+				return i;
+		}
+		return nums.length;
+	}
 
-  public int searchInsert(int[] nums, int target) {
-    int left = 0, right = nums.length - 1;
-    int mid = 0;
-    while (left <= right) {
-      mid = (left + right) / 2;
-      if (nums[mid] == target)
-        return mid;
-      if (nums[mid] < target)
-        left = mid + 1;
-      else
-        right = mid - 1;
-    }
-    return left;
-  }
+	public int searchInsert(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		int mid = 0;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (nums[mid] == target)
+				return mid;
+			if (nums[mid] < target)
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		return left;
+	}
 }
