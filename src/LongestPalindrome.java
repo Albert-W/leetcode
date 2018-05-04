cfacc23f98a7904bdd05e8a654568a0d2e8b83aa
Albert-W <wuyichang1990@gmail.com>
2018-05-04 11:36:24 +0100

    4. Median of Two Sorted Arrays

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,3 +1,17 @@
+/*
+5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+ */
 public class LongestPalindrome {
     public String longestPalindrome(String s) {
         int sum = 1;
