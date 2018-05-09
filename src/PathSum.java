7ba40178384becff8af7d408bddbe16d84f5afdc
Albert-W <wuyichang1990@gmail.com>
2018-05-09 15:08:54 +0100

    112. Path Sum

--- src/PathSum.java
+++ src/PathSum.java
@@ -19,7 +19,7 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 
  */
-public class HasPathSum {
+public class PathSum {
     //DFS, recurrence
     public boolean hasPathSum(TreeNode root, int sum) {
         //first, the outlet.
