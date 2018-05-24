7820ec56548600b9e1512d77da33448ee8906ffa
Albert-W <wuyichang1990@gmail.com>
2018-05-24 20:02:38 +0100

    338. Counting Bits
    Signed-off-by: Albert-W <wuyichang1990@gmail.com>

---
+++ src/CountBits.java
@@ -0,0 +1,44 @@
+/*
+338. Counting Bits
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+ */
+//bit manipulation
+public class CountBits {
+    //make full use of the past;3ms;
+    // non negative
+    public int[] countBits2(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i < num + 1; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+
+
+    //6ms;
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i < num + 1; i++) {
+            ans[i] = cBits(i);
+        }
+        return ans;
+    }
+
+    int cBits(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            num = num & (num - 1);
+        }
+        return count;
+    }
+}
