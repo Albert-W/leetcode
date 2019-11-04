43bc425e9d8c99d24e33a326a9a34494f839cc9b
Albert <wuyichang1990@gmail.com>
2019-11-04 22:10:45 +0000

    add

--- vscodeExt/279.perfect-squares.py
+++ vscodeExt/279.perfect-squares.py
@@ -8,41 +8,71 @@
 
 # import math
 class Solution:
-    # Time Limit Exceeded
-    def numSquares_E1(self, n: int) -> int:
-        res = [n] * (n+1)
-        res[1] = 1
-        i = 1
-        while i <=n:
-            sqrt = int(i**0.5)
-            if sqrt ** 2 == i:
-                res[i]=1
-            else :
-                j = 1
-                while j <= i//2:
-                    res[i]= min(res[j]+ res[i-j],res[i]);
-                    j+=1    
 
-            i+=1
-        return res[n]    
-
+    # 必须利用最优子结构，即从小开始往大记录。  
     # Time Limit Exceeded
-    # 8328
+    # 8405
+    # res[i]= min(res[i-j*j]) + 1 。 
     def numSquares(self, n: int) -> int:
-        res = [n] * (n+1)
+        res = [n]*(n+1)
+        res[0] = 0
         res[1] = 1
         i = 1
-        while i*i <=n:
-            res[i*i]=1
-            i += 1    
-        i =1 
-        while i<=n :   
+        while i**2 <= n:
+            res[i**2] = 1
+            i += 1
+        while i<= n:
             j = 1
-            while i+ j*j <= n:
-                res[i + j * j]= min(res[i]+ 1,res[i + j * j]);
-                j+=1    
+            while j*j <= i:
+                res[i] = min(res[i-j*j] + 1, res[i])
+                j+=1
             i+=1
-        return res[n]
-        
+        return res[n]             
+
+
+    # 使用了静态空间通过了。 
+    _dp = [0]
+    def numSquares(self, n):
+        dp = self._dp
+        while len(dp) <= n:
+            dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
+        return dp[n]              
+
+    # BFS 宽度有限的遍历， 
+    # 队列首部是步数最小的情型。 
+    # 增加step，减少num. 一增一减。
+    def numSquares(self, n: int) -> int:
+        q = [] # list(num -> steps)
+        q.append((n, 0)) # take zero step to arrive n 
+        # 247 -> 7927 队列去重之后的性能提升。
+        visited = [False]*(n+1)
+        visited[n] = True
+
+        while len(q):
+            num = q[0][0]
+            steps = q[0][1]
+            del q[:1]
+
+            if num == 0 : 
+                return steps # take steps to arrive zero
+
+            i = 0 
+            while 1 :
+                a = num - i**2
+                if a<0:
+                    break
+                # 判断提前之后，成功通过了, 用时996ms 
+                # 变量计算统一为a,之后，用时768ms
+                # list 改为tuple 之后，用时576ms
+                 
+                if a == 0:
+                    return steps + 1
+                if not visited[a]:
+                    q.append((a, steps + 1));
+                    visited[a]=True
+                i+=1    
+                          
+
+
 # @lc code=end
 
---
+++ vscodeExt/347.top-k-frequent-elements.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode id=347 lang=python3
+#
+# [347] Top K Frequent Elements
+#
+
+# @lc code=start
+import heapq
+class Solution:
+    def topKFrequent_1(self, nums: List[int], k: int) -> List[int]:
+        assert k> 0 
+        # 统计频率
+        # d (num, freq)
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        # 维护频率最高的k个元素。 
+        # priority_queue（freq, number）
+        q = []
+
+        for key, value in d.items():
+            if len(q) == k:
+                # 需要比较大小，移出较小的
+                # q与第一个数比较
+                if value > q[0][0]:
+                    heapq.heappop(q)
+                    heapq.heappush(q,(value,key))
+            elif len(q) < k:
+                heapq.heappush(q,(value,key))
+
+        res = []
+        for i in q:
+            print(i)
+            res[:0]=(i[1],)
+        return res            
+                        
+    # 顺序相反，也可以pass. 
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #counter ( value: counts )
+        counter = collections.Counter(nums)
+        queue = []
+        for n in counter:
+            # q (counts, value)
+            heapq.heappush(queue,(counter[n],n))
+            if len(queue) > k:
+                heapq.heappop(queue)
+        res = []
+        # 展开了tuple, 取第二个。 
+        for c, n in queue:
+            print(c,n)
+            res.append(n)
+        return res
+
+
+# @lc code=end
+
