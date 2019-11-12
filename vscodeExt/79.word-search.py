903b2bf1bd0d56927203fb4ff87b28dce2e607de
Albert <wuyichang1990@gmail.com>
2019-11-12 10:59:55 +0000

    add

--- vscodeExt/79.word-search.py
+++ vscodeExt/79.word-search.py
@@ -7,24 +7,26 @@
 # @lc code=start
 class Solution:
     d = [(-1,0),(0,1),(1,0),(0,-1)]
-    isVisited = []
     m = 0
     n = 0
-
 
     def exist(self, board: List[List[str]], word: str) -> bool:
         if len(board) == 0 :
             return False
         self.m = len(board)
         self.n = len(board[0])
-        self.isVisited = [[False]*self.n]*self.m 
-        print(self.isVisited)
+        # self.isVisited = [[False]*self.n]*self.m 
+        # self.isVisited = [[ False for j in range(self.n) ] for i in range(self.m)]
+        # print(self.isVisited)
         for i in range(self.m):
             for j in range(self.n):
                 # 如果找到了就返回True
                 # 没找到就循环找
-                if self.findWord(board, word, i,j):
-                    return True
+                # 把第一步判断提到外层，不要递归
+                if board[i][j] == word[0]:
+                    if self.findWord(board, word, i,j):
+                        return True
+
         return False
 
 
@@ -32,20 +34,26 @@ class Solution:
         return x>= 0 and x<self.m and y >=0 and y< self.n
 
     def findWord(self, board, word, x, y):
-        if board[x][y] != word[0]:
+        # 如果不匹配，或被访问过，返回False.
+        if board[x][y] != word[0]: 
             return False
+            
         # 定义递归出口
-        if len(word)==1 and board[x][y] == word[0]:
-            return True  
-        self.isVisited[x][y] == True    
+        if len(word)==1 :
+            return board[x][y] == word[0]
+        # 进入递归体 误写了==号   
+        board[x][y] = None  
+        # print(self.isVisited)
         for i in range(4):
             newX = x + self.d[i][0]
             newY = y + self.d[i][1]
-            print(newX, newY)
-            if self.isValid(newX, newY) and self.isVisited[newX][newY] == False:
-                return self.findWord(board,word[1:], newX, newY)
-        # 四周没找到，回滚
-        self.isVisited[x][y] == False        
+            # print(newX, newY,word[1:])
+            if self.isValid(newX, newY) and self.findWord(board,word[1:], newX, newY):
+                
+                return True
+        # 四周没找到，回滚，误写了==号
+        board[x][y] = word[0]       
+        return False
 
 
         
