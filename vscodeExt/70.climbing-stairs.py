35b06107aae383b85785519e9d2001bdbe6d5e0e
Albert <wuyichang1990@gmail.com>
2019-11-22 23:05:52 +0000

    amend

--- vscodeExt/70.climbing-stairs.py
+++ vscodeExt/70.climbing-stairs.py
@@ -7,15 +7,29 @@
 # @lc code=start
 class Solution:
     def climbStairs_E1(self, n: int) -> int:
-        # dynamic programming 
+        # This is not dynamic programming 
         # Time Limit Exceeded
         if n == 1:
             return 1
         if n == 2:
             return 2
         return self.climbStairs(n-2) + self.climbStairs(n-1)   
-        
+
+
     def climbStairs(self, n: int) -> int:
+        # dynamic programming
+        if n == 1:
+            return 1        
+        list = [-1]*(n+1);
+        list[0] = 0;
+        list[1] = 1;
+        list[2] = 2;
+        for i in range(3,n+1):
+            list[i] = list[i-2]  + list[i-1];
+            
+        return list[n]
+
+    def climbStairs_1(self, n: int) -> int:
     # iteration
 
         if n == 1:
