1d912b5a97428bfb5ded518fe262bedb9a0e32a2
Albert <wuyichang1990@gmail.com>
2019-12-10 15:46:02 +0000

    343

--- vscodeExt/343.integer-break.py
+++ vscodeExt/343.integer-break.py
@@ -6,20 +6,23 @@
 
 # @lc code=start
 class Solution:
+    # 函数入口
+    # 动态规划，需要两部分。
     def integerBreak(self, n: int) -> int:
         if n < 2:
             return -1  
-        # list[n-1] 就是 n可以得到的最大值
-        list = [-1] * n
-        # print(list)
-        list[0] = 1
+        # list[n] 就是 n可以得到的最大值,方便下标使用
+        list = [0] * (n+1)
         list[1] = 1
-        # list[2] = 2
-        max = 0
-        for i in range( (n-1)//2):
-            if list[i] * list[n-1-i] > max:
-                max = list[i] * list[n-1-i]
-        return max 
+        list[2] = 1
+        # max = 0
+        # 从3算到n, 规模从小到大。
+        for i in range(3, n+1):
+            # 把i 分为两部分 j, i-j
+            for j in range(1,i):
+                # 存在i-j < list[i-j]的情况。
+                list[i] = max( j* max(i-j,list[i-j]) , list[i] )
+        return list[n]
         
 # @lc code=end
 
