fc31cd00ad26281bbe4ecc93692f2e5b7beb7d79
Albert <wuyichang1990@gmail.com>
2019-12-15 23:34:50 +0000

    202

--- vscodeExt/202.happy-number.py
+++ vscodeExt/202.happy-number.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def isHappy(self, n: int) -> bool:
+        # 记录过程，防止产生循环。
         s = set()
         while n not in s:
             s.add(n)
@@ -18,7 +19,7 @@ class Solution:
             if sum == 1:
                 return True
             n = sum         
-            
+        # 构成循环，返回false 
         else:
             return False
 # @lc code=end
