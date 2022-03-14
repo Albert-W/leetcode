c1bd26118c3d1080cc595a84f0b9f70436d6be57
Albert-W <919911537@qq.com>
2022-03-14 23:13:53 +0000

    add 292

---
+++ vscodeExt/292.nim-game.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=292 lang=java
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
+// @lc code=end
+
