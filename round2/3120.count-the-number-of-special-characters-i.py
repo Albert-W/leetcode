9bd42e993cbf98b36b1716ae23a6d53cecda8c29
Yichang Wu <yichangw1@vmware.com>
2024-08-13 21:03:27 +0100

    add 3120

---
+++ round2/3120.count-the-number-of-special-characters-i.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=3120 lang=python
+#
+# [3120] Count the Number of Special Characters I
+#
+
+# @lc code=start
+class Solution(object):
+    def numberOfSpecialChars(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        # ord('a') = 97, ord('A') = 65
+        s = set()
+        for i in word:
+            if ord(i) > 96 and i.upper() in word:
+                s.add(i)
+        return len(s)
+
+        
+# @lc code=end
+
---
+++ round2/3121.count-the-number-of-special-characters-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=3121 lang=python
+#
+# [3121] Count the Number of Special Characters II
+#
+
+# @lc code=start
+class Solution(object):
+    def numberOfSpecialChars(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        
+# @lc code=end
+
